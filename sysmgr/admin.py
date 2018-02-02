30f5116645d3e035c9a61be798b1db66149ed05a
oyq欧阳琼 <qouyang@ctrip.com>
2018-02-02 19:39:55 +0800

    add region menu provider

--- sysmgr/admin.py
+++ sysmgr/admin.py
@@ -17,10 +17,45 @@ class GroupAdmin(admin.ModelAdmin):
     list_display = ('name', 'pid') # list
     filter_horizontal = ('user', 'role',)
 
+class RoleMenuInline(admin.TabularInline):
+    model = models.RoleMenuPermission
+    extra = 0
+
+class RoleRegionInline(admin.TabularInline):
+    model = models.RoleRegionPermission
+    extra = 0
+
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'desc') # list
+    inlines = (RoleMenuInline, RoleRegionInline, )
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'pid', 'menu_type', 'code', 'name',
+        'desc', 'addr', 'addr_param', 'i_class', 'sort', 'is_active')
+    search_fields = ("code", "name",)
+    list_filter = ('menu_type',)
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'desc') # list
+    filter_horizontal = ('menu',)
+
+class RegionInline(admin.TabularInline):
+    model = models.Region
+    extra = 0
+
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'desc') # list
+    inlines = (RegionInline, )
+
+
 
 # Register your models here.
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Group, GroupAdmin)
-admin.site.register(models.Role)
+admin.site.register(models.Role, RoleAdmin)
+admin.site.register(models.Menu, MenuAdmin)
+admin.site.register(models.Region, RegionAdmin)
+admin.site.register(models.Provider, ProviderAdmin)
 admin.site.disable_action('delete_selected')
 
--- sysmgr/models.py
+++ sysmgr/models.py
@@ -1,9 +1,13 @@
 # coding=utf-8
 from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 
+
+@python_2_unicode_compatible
 class Role(models.Model):
     id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=10, default='')
     name = models.CharField(max_length=30, default='')
     desc = models.CharField(max_length=200, default='')
     is_active = models.BooleanField(default=True)
@@ -12,6 +16,7 @@ class Role(models.Model):
         return self.name
 
 
+@python_2_unicode_compatible
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     login_name= models.CharField(max_length=30, default='')
@@ -28,10 +33,12 @@ class User(models.Model):
         return self.username
 
 
-
+@python_2_unicode_compatible
 class Group(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=30, default='')
+    code = models.CharField(max_length=6, default='')
+    name = models.CharField(max_length=20, default='')
+    desc = models.CharField(max_length=50, default='')
     pid = models.ForeignKey('self', null=True, blank=True, verbose_name="parent group")
     user = models.ManyToManyField(User)
     role = models.ManyToManyField(Role)
@@ -39,4 +46,70 @@ class Group(models.Model):
     def __str__(self):
         return self.name
 
+@python_2_unicode_compatible
+class Menu(models.Model):
+    MENU_TYPE = (
+        ("dashboard", "dashboard"),
+        ("console", "console"),
+    )
+    id = models.AutoField(primary_key=True)
+    pid = models.ForeignKey('self', null=True, blank=True, verbose_name="parent menu")
+    code = models.CharField(max_length=6, default='')
+    name = models.CharField(max_length=20, default='')
+    desc = models.CharField(max_length=50, default='')
+    addr = models.CharField(max_length=150, blank=True, null=True)
+    addr_param = models.CharField(max_length=50, blank=True)
+    i_class = models.CharField(max_length=100, null=True, blank=True)
+    sort = models.IntegerField(blank=True, null=True)
+    menu_type = models.CharField(max_length=20, default="console", choices=MENU_TYPE)
+    is_active = models.BooleanField(default=True)
 
+    meta = {
+        "ordering": ['sort']
+    }
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
+class Provider(models.Model):
+    CLOUD_TYPE = (
+        ("private", "private"),
+        ("private", "public"),
+    )
+    id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=6, default='')
+    name = models.CharField(max_length=20, default='')
+    desc = models.CharField(max_length=50, default='')
+    cloud_type = models.CharField(max_length=20, default="private", choices=CLOUD_TYPE)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "{0}|{1}".format(self.code, self.name)
+
+
+@python_2_unicode_compatible
+class Region(models.Model):
+    id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=6, default='')
+    name = models.CharField(max_length=20, default='')
+    desc = models.CharField(max_length=50, default='')
+    is_active = models.BooleanField(default=True)
+    provider = models.ForeignKey(Provider, to_field="id")
+    menu = models.ManyToManyField(Menu, null=True, blank=True)
+
+    def __str__(self):
+        return "{0}|{1}".format(self.code,self.name)
+
+
+class RoleMenuPermission(models.Model):
+    id = models.AutoField(primary_key=True)
+    role = models.ForeignKey(Role, to_field="id")
+    menu = models.ForeignKey(Menu, to_field="id")
+
+
+class RoleRegionPermission(models.Model):
+    id = models.AutoField(primary_key=True)
+    role = models.ForeignKey(Role, to_field="id")
+    region = models.ForeignKey(Region, to_field="id")
